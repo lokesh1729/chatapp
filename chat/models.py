957492a1aa9b19afa45c12f0b1b91c9819c75102
Sanapalli Lokesh <slokesh@akamai.com>
2019-07-01 14:57:16 +0530

    refactoring models and adding migrations

--- chat/models.py
+++ chat/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 
 USER = get_user_model()
+
 
 # Create your models here.
 class BaseModel(models.Model):
@@ -10,30 +11,39 @@ class BaseModel(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 
+class ChatRoom(BaseModel):
+    name = models.CharField(_("Name of the room"), max_length=255, primary_key=True)
+
+    def __str__(self):
+        return "%s" % self.name
+
+
+class Participant(BaseModel):
+    rooms = models.ManyToManyField(
+        ChatRoom, related_name="participants",
+        related_query_name="participant"
+    )
+    user = models.ForeignKey(USER, on_delete=models.CASCADE, related_name="participants",
+                             related_query_name="participant")
+
+    def __str__(self):
+        return "%s : %s" % (self.rooms.all(), self.user.username)
+
+
 class Message(BaseModel):
     text = models.TextField(blank=True, null=True)
     sender = models.ForeignKey(
-        USER,
+        Participant,
         on_delete=models.CASCADE,
         related_name="sent_messages",
         related_query_name="sent_message",
     )
-    receiver = models.ForeignKey(
-        USER,
+    room = models.ForeignKey(
+        ChatRoom,
         on_delete=models.CASCADE,
-        related_name="received_messages",
-        related_query_name="received_message",
+        related_name="messages",
+        related_query_name="message",
     )
 
-
-class Room(BaseModel):
-    name = models.CharField(_("Name of the room"), max_length=255)
-    room_message = models.ForeignKey(
-        Message,
-        on_delete=models.CASCADE,
-        related_name="rooms",
-        related_query_name="room",
-    )
-    participant = models.ForeignKey(
-        USER, on_delete=models.CASCADE, related_name="rooms", related_query_name="room"
-    )
+    def __str__(self):
+        return "%s : %s" % (self.sender, self.room)
