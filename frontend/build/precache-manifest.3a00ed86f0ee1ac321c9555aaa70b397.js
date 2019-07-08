bd4addef1e75d6577481139fb9af818adb273b78
lokesh1729 <s.lokesh1729@gmail.com>
2019-07-08 09:37:19 +0530

    adding frontend build

---
+++ frontend/build/precache-manifest.3a00ed86f0ee1ac321c9555aaa70b397.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ede3d68677c5f0843ca9cd3e7d46e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bc824e36695b69bbe19",
+    "url": "/static/css/main.e66a5044.chunk.css"
+  },
+  {
+    "revision": "db0618a9a0a3aa9dfd04",
+    "url": "/static/js/2.ad519b72.chunk.js"
+  },
+  {
+    "revision": "0bc824e36695b69bbe19",
+    "url": "/static/js/main.866fb16e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
