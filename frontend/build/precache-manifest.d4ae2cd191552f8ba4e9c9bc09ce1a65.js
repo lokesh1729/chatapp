11c890fc953a762e98645bb98ecf77f98305f01c
lokesh1729 <s.lokesh1729@gmail.com>
2019-07-08 21:51:01 +0530

    adding static js files

---
+++ frontend/build/precache-manifest.d4ae2cd191552f8ba4e9c9bc09ce1a65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3553e74b030b0f2d0e3171ddf6d9cb7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a82692014aa238e178a4",
+    "url": "/static/css/main.e66a5044.chunk.css"
+  },
+  {
+    "revision": "db0618a9a0a3aa9dfd04",
+    "url": "/static/js/2.ad519b72.chunk.js"
+  },
+  {
+    "revision": "a82692014aa238e178a4",
+    "url": "/static/js/main.a7e541e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
