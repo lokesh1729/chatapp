a8e6e6d156503273d403a0841b411d48fc10196b
lokesh1729 <s.lokesh1729@gmail.com>
2019-07-18 11:57:55 +0530

    chatapp-11 - chatapp : implement signup page

--- chatapp/users/models.py
+++ chatapp/users/models.py
@@ -2,11 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
-from django.conf import settings
-from django.contrib.auth import hashers
 from django.utils.translation import ugettext_lazy as _
 
-user_model_label = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 class User(AbstractUser):
 
@@ -14,51 +11,10 @@ class User(AbstractUser):
     # around the globe.
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
     last_seen = models.DateTimeField(_("Last Seen"), default=timezone.now)
+    security_question1 = models.TextField(blank=True, null=True)
+    security_question2 = models.CharField(blank=True, null=True, max_length=255)
+    security_answer1 = models.TextField(blank=True, null=True)
+    security_answer2 = models.CharField(blank=True, null=True, max_length=255)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
-
-
-class SecurityQuestion(models.Model):
-    users = models.ManyToManyField(user_model_label,
-                             related_name="security_questions",
-                             related_query_name="security_question")
-    question = models.TextField(null=False, blank=False)
-
-    def __unicode__(self):
-        return _("%s - %s") % (self.users, self.question)
-
-
-class SecurityAnswer(models.Model):
-    user = models.ForeignKey(user_model_label, on_delete=models.CASCADE,
-                             related_name="security_answers", related_query_name="security_answer")
-    question = models.ForeignKey(SecurityQuestion, verbose_name=_("Security Question"),
-                                 on_delete=models.CASCADE, related_name="security_answers",
-                                 related_query_name="security_answer")
-    answer = models.CharField(max_length=100, null=False, blank=False)
-
-    def __unicode__(self):
-        return _("%s - %s") % (self.user, self.question)
-
-    def hash_current_answer(self):
-        self.set_answer(self.answer)
-
-    def set_answer(self, raw_answer):
-        if not bool(getattr(settings, "SECURITY_QUESTIONS_CASE_SENSITIVE", False)):
-            raw_answer = raw_answer.upper()
-        self.answer = hashers.make_password(raw_answer)
-
-    def check_answer(self, raw_answer):
-        if not bool(getattr(settings, "SECURITY_QUESTIONS_CASE_SENSITIVE", False)):
-            raw_answer = raw_answer.upper()
-
-        def setter(raw_answer):
-            self.set_answer(raw_answer)
-            self.save(update_fields=["answer"])
-        return hashers.check_password(raw_answer, self.answer, setter)
-
-    def set_unusable_answer(self):
-        self.answer = hashers.make_password(None)
-
-    def has_usable_answer(self):
-        return hashers.is_password_usable(self.answer)
--- chatapp/users/serializers.py
+++ chatapp/users/serializers.py
@@ -2,16 +2,10 @@ from rest_framework import serializers
 from rest_framework import exceptions
 from django.contrib.auth import get_user_model, authenticate
 
-from chatapp.users.models import SecurityQuestion, SecurityAnswer
-
 USER = get_user_model()
 
-class RegisterSerializer(serializers.ModelSerializer):
-    security_question1 = serializers.CharField()
-    security_question2 = serializers.CharField()
-    security_answer1 = serializers.CharField()
-    security_answer2 = serializers.CharField()
 
+class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = USER
         fields = ("first_name", "last_name", "username", "email", "password",
@@ -19,10 +13,8 @@ class RegisterSerializer(serializers.ModelSerializer):
                   "security_answer2")
         extra_kwargs = {"password": {"write_only": True},
                         "email": {"required": False},
-                        "security_question1": {"required": False, "write_only": True},
-                        "security_question2": {"required": False, "write_only": True},
-                        "security_answer1": {"required": False, "write_only": True},
-                        "security_answer2": {"required": False, "write_only": True},
+                        "security_answer1": {"write_only": True},
+                        "security_answer2": {"write_only": True},
                         }
 
     def create(self, validated_data):
@@ -33,19 +25,11 @@ class RegisterSerializer(serializers.ModelSerializer):
                 validated_data["password"],
                 first_name=validated_data["first_name"],
                 last_name=validated_data["last_name"],
+                security_question1=validated_data["security_question1"],
+                security_answer1=validated_data["security_question1"],
+                security_question2=validated_data["security_question2"],
+                security_answer2=validated_data["security_answer2"]
             )
-            if validated_data.get("security_question1") and validated_data.get("security_answer1"):
-                security_question_obj = SecurityQuestion.objects.create(question=validated_data[
-                    "security_question1"])
-                security_question_obj.users.add(user)
-                SecurityAnswer.objects.create(user=user, question=security_question_obj,
-                                              answer=validated_data["security_answer1"])
-            if validated_data.get("security_question2") and validated_data.get("security_answer2"):
-                security_question_obj = SecurityQuestion.objects.create(question=validated_data[
-                    "security_question2"])
-                security_question_obj.users.add(user)
-                SecurityAnswer.objects.create(user=user, question=security_question_obj,
-                                              answer=validated_data["security_answer2"])
         except KeyError as exc:
             raise exceptions.ValidationError("Missing %s in request body" % exc)
 
@@ -59,7 +43,6 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class LoginSerializer(serializers.Serializer):
-
     username = serializers.CharField()
     password = serializers.CharField()
 
